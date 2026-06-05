42497a484229563a9301b9295290550ef305ab43
EdwinDevan <51376396+EdwinDevan@users.noreply.github.com>
2026-06-05 19:41:04 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("==========================================");
+        System.out.println("  DEVOPS INTERNAL ASSESSMENT: SUCCESS!   ");
+        System.out.println("  Hello from Stanley's Ubuntu Runner Node! ");
+        System.out.println("==========================================");
+    }
+}
