94a2840900d41d2d00317ad12aa393573a7744a3
EdwinDevan <51376396+EdwinDevan@users.noreply.github.com>
2026-06-05 20:52:23 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,7 +1,7 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;1
 
 @SpringBootApplication
 @RestController
